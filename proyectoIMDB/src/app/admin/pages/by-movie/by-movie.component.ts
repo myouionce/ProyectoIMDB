e8aa52cb4e2173c131f102dc7e3472d89f6244c7
cherry_motion <dlmora348@gmail.com>
2025-02-03 00:12:26 -0600

    update

--- proyectoIMDB/src/app/admin/pages/by-movie/by-movie.component.ts
+++ proyectoIMDB/src/app/admin/pages/by-movie/by-movie.component.ts
@@ -3,16 +3,24 @@ import {MatButtonModule} from '@angular/material/button'
 import { MoviesTableComponent } from '../../components/movies-table/movies-table.component';
 import { MovieService } from '../../../shared/services/movie.service';
 import { Pelicula } from '../../../shared/interfaces/imdb.interface';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 
 @Component({
   selector: 'app-by-movie',
-  imports: [MatButtonModule, MoviesTableComponent],
+  imports: [MatButtonModule, MoviesTableComponent, MatIconModule, MatInputModule,
+      MatFormFieldModule, ReactiveFormsModule],
   templateUrl: './by-movie.component.html',
   styleUrl: './by-movie.component.scss'
 })
 export class ByMovieComponent {
   public MovieData!:Pelicula[];
+
+  public searchInput = new FormControl();
+
   constructor(
       private movieService:MovieService
   
@@ -25,5 +33,13 @@ export class ByMovieComponent {
         console.log(this.MovieData);
       });
   }
+
+  searchPelicula(): void{
+    const value: string = this.searchInput.value || '';
+
+    this.movieService.getFilteredMovies('', value, '', '', 'titulo', 'desc')
+      .subscribe( actores => this.MovieData = actores );
+  }
+
 
 }
